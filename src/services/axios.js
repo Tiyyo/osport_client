bef6227fd0935fe88250b9e0aa12f435fc45f42c
Guillaume <jolibois.contact@gmail.com>
2023-08-18 14:47:21 +0200

    :hammer: eslint with airbnb implemented

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,29 +1,30 @@
-import axios from 'axios'
-import { VITE_API_SERVER_URL } from '../env'
+import axios from 'axios';
 
-// l'idée c'est de créer une instance d'axios préconfigurée que l'on pourra utiliser partout dans notre code
+// l'idée c'est de créer une instance d'axios préconfigurée que l'on pourra
+// utiliser partout dans notre code
 
-
-// on crée une instance avec une URL de base, on aura juste à compléter l'URL au besoin, lorsqu'on effectuera des requêtes
-// ex : axiosInstance.get('/recipes') fera une requete sur ...
-export const axiosInstance = axios.create({
-    baseURL: VITE_API_SERVER_URL
-})
+// on crée une instance avec une URL de base, on aura juste à compléter l'URL au
+// besoin, lorsqu'on effectuera des requêtes ex : axiosInstance.get('/recipes')
+// fera une requete sur ...
+const axiosInstance = axios.create({
+  baseURL: process.env.local.VITE_API_SERVER_URL,
+});
 
 // on peut aussi utiliser un middleware pour configurer notre requête avant de l'envoyer
 axiosInstance.interceptors.request.use((config) => {
+  // ici le MW essaye de récupérer le user dans le storage
+  const user = JSON.parse(localStorage.getItem('user'));
 
-    // ici le MW essaye de récupérer le user dans le storage
-    const user = JSON.parse(localStorage.getItem('user'))
+  console.log(user);
 
-    console.log(user);
+  // si il le trouve, il ajoute un headers Authorization avec comme valeur le token du localstorage
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`;
+  }
 
-    // si il le trouve, il ajoute un headers Authorization avec comme valeur le token du localstorage
-    if (user) {
-        config.headers.Authorization = `Bearer ${user.token}`
-    }
+  return config;
+});
 
-    return config;
-})
+export default axiosInstance;
 
 // * On utilise cette instance dans userActions.js et recipesActions.js
