3df9150f620a971f6c7052c6ce08ceff3de59d22
Thomas GOUEL <thomasgouel@macbook-air-de-thomas.home>
2023-08-18 18:38:12 +0200

    :tada: Page Login intégrée

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import LogIn from "../LogIn/LogIn";
 import SignUp from "../Signup/SignUp";
 import Profile from "../Profile/Profile";
+import "./App.css";
+import { Routes , Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <LogIn />
-      <SignUp />
-      <Profile />
-    </>
+    <Routes>
+      <Route path="/login" element={<LogIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/" element={<Profile />} />
+    </Routes>
   );
 }
 
--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function LogIn() {
+interface LogInProps {} // Ceci est une interface vide car LogIn n'accepte aucune props
+
+const LogIn: React.FC<LogInProps> = () => {
   return (
-    <div>LogIn</div>
-  )
-}
+    <>
+      <div className="flex justify-center m-1">
+        <img
+          className="w-24 h-24"
+          src="../../../public/imageedit_3_3008038748.png"
+          alt="logo"
+        />
+      </div>
 
-export default LogIn
+      <div className="text-2xl font-bold text-center m-1 mb-6">
+        Se connecter à O'Sport
+      </div>
+
+      <div className="card w-96 bg-base-100 shadow-xl border border-gray-700 m-1 mb-6">
+        <div className="form-control w-full max-w-xs m-1">
+          <label className="label">
+            <span className="label-text">Nom d'utilisateur : </span>
+          </label>
+          <input
+            type="text"
+            placeholder="Tapez ici"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+
+        <div className="form-control w-full max-w-xs m-1">
+          <label className="label">
+            <span className="label-text">Mot de passe : </span>
+          </label>
+          <input
+            type="password"
+            placeholder="Tapez ici"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </div>
+
+        <Link to="" className="link link-info m-1">
+          Mot de passe oublié ?
+        </Link>
+
+        <button className="btn btn-outline btn-success m-1">
+          Se connecter
+        </button>
+      </div>
+
+      <div className="container border border-white rounded-lg m-1 p-3">
+        Nouveau sur O'Sport ? &nbsp;
+        <Link to="/signup" className="link link-info">
+          Créer un compte
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default LogIn;
