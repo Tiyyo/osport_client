c77765e3cb083ae8e10e181db762c2bce238636e
Anthony Bourret <anthony.bourret@gmail.com>
2023-08-21 15:59:16 +0200

    :tada: - Menu added

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Menu() {
   return (
     <nav className="flex gap-4 p-4 justify-center">
-      <button type="button" className="btn">Profile</button>
-      <button type="button" className="btn">Contact</button>
-      <button type="button" className="btn">Event</button>
+      <button type="button" className="btn"><Link to="/">Profile</Link></button>
+      <button type="button" className="btn"><Link to="/contact">Contact</Link></button>
+      <button type="button" className="btn"><Link to="/event_list">Event</Link></button>
     </nav>
   );
 }
