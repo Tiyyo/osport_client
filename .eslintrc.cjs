ba68a70a2a83db31222d2380a9f20b281c4bde65
Thomas GOUEL <thomasgouel@macbook-air-de-thomas.home>
2023-08-22 10:34:30 +0200

    :construction: Minor changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,44 +6,36 @@ module.exports = {
     "airbnb",
     "airbnb-typescript",
     // react-hooks = List of rules present in : https://legacy.reactjs.org/docs/hooks-rules.html
-    "plugin:react-hooks/recommended",
+    "plugin:react-hooks/recommended"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: {
-    project: `${__dirname}/tsconfig.json`,
+    project: `${__dirname}/tsconfig.json`
   },
   plugins: ["react-refresh", "react-hooks"],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-
-    "react/jsx-filename-extension": [
-      1,
-      { extensions: [".js", ".jsx", ".tsx"] },
-    ],
-
+    "@typescript-eslint/indent": "off",
+    "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".tsx"] }],
     "no-console": "off",
-
     "jsx-a11y/label-has-associated-control": [
       "error",
       {
         required: {
-          some: ["nesting", "id"],
-        },
-      },
+          some: ["nesting", "id"]
+        }
+      }
     ],
 
     "jsx-a11y/label-has-for": [
       "error",
       {
         required: {
-          some: ["nesting", "id"],
-        },
-      },
+          some: ["nesting", "id"]
+        }
+      }
     ],
 
-    "react/no-unescaped-entities": 0,
-  },
+    "react/no-unescaped-entities": 0
+  }
 };
