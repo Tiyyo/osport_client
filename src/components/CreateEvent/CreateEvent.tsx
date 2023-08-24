28e801b5b05f78a5cc8ef79502524abfbfbd5440
Anthony Bourret <anthony.bourret@gmail.com>
2023-08-24 15:15:39 +0200

    :hammer: Menu Repaired - Z-index added

--- src/components/CreateEvent/CreateEvent.tsx
+++ src/components/CreateEvent/CreateEvent.tsx
@@ -10,11 +10,11 @@ function CreateEvent() {
     <>
       <Header />
       <Menu />
-      <div className="flex flex-col items-center m-4 sm:w-4/5 sm:p-4 sm:m-auto sm:shadow-xl sm:border sm:rounded-xl sm:border-gray-700 sm:pb-4 sm:border-2 sm:mt-4 gap-4">
+      <div className="flex flex-col items-center m-4 sm:w-4/5 sm:p-4 sm:m-auto sm:shadow-xl sm:border sm:rounded-xl sm:border-gray-700 sm:pb-4 sm:border-2 sm:mt-4 gap-4  mb-8">
         <h1 className="text-3xl py-2 sm:self-start">New event</h1>
         <CreateEventForm />
         <div className="divider px-20" />
-        <div className="flex flex-col w-full sm:flex-row">
+        <div className="flex flex-col w-full sm:flex-row z-0">
           <EventContactList />
           <div className="divider pt-10 px-20" />
           <SendInvitations />
