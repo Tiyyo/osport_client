ca5562458fa993d9087bcb156d2929b60c0d6df9
lhp9-tg <thomas.gouel@gmail.com>
2023-08-28 12:28:51 +0200

    :hammer: Minor changes

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -13,29 +13,9 @@ function Profile() {
   const { user } = useContext(AuthContext);
   console.log(user);
 
-
-
-  // try {
-  //   // Submit data to the servers
-  //   const response = axiosInstance.get('/user/validate', { withCredentials: true });
-
-  //   console.log(response);
-
-  //   // if (response.status === 200 && response.data.error !== 'Invalid input') {
-  //   //   login({ name: cleanUsername });
-  //   //   navigate('/');
-  //   // } else {
-  //   //   setErrorMessages(['Une erreur inattendue s\'est produite.']);
-  //   //   setisServerValid(false);
-  //   // }
-  // } catch (e: any) {
-  //     console.error('Une erreur s\'est produite lors de l\'attente de la réponse', e);
-  // }
-
-
   return (
     <>
-      {/* <Header />
+      <Header />
       <Menu />
       <div className="flex flex-col px-4 my-auto sm:w-4/5 sm:m-auto sm:shadow-xl sm:border sm:rounded-xl sm:border-gray-700 sm:my-4 sm:pb-4">
         <div className="flex flex-col gap-4 py-4 sm:flex-row">
@@ -43,7 +23,7 @@ function Profile() {
           <EventListPreview />
         </div>
         <NextEvent />
-      </div> */}
+      </div>
     </>
   );
 }
