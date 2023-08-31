20924af4ab612c1a9c4afce2d7d1652f8b5ec7d8
lhp9-tg <thomas.gouel@gmail.com>
2023-08-31 21:35:32 +0200

    :tada: Edit lvl quasi terminée attente de modif back + useMutate à finir

--- src/components/EditProfile/EditProfile.tsx
+++ src/components/EditProfile/EditProfile.tsx
@@ -10,9 +10,9 @@ function EditProfile() {
       <Header />
       <Menu />
       <div className="flex flex-col p-4 mb-28 sm:w-4/5 sm:m-auto sm:shadow-xl sm:rounded-xl sm:gap-4 sm:border-gray-700 sm:my-4 sm:pb-4 sm:flex-row sm:justify-between">
-        <EditInfo />
+        {/* <EditInfo /> */}
         <div className="divider" />
-        {/* <EditLevel /> */}
+        <EditLevel />
       </div>
     </>
   );
---
+++ src/components/EditProfile/LevelSelector/SportRanking.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import useMutation from '../../hooks/useMutation';
+
+interface SportRankingProps {
+    sportSelected: string;
+    ownRank: number | undefined;
+  }
+
+function SportRanking ( { sportSelected, ownRank } : SportRankingProps) {
+
+    const [level, setLevel] = useState<string>('Choice');
+    const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
+    console.log(level);
+
+    useEffect(() => {
+      switch (ownRank) {
+        case 2:
+          setLevel('Beginner');
+          break;
+        case 6:
+          setLevel('Intermediate');
+          break;
+        case 8:
+          setLevel('Confirmed');
+          break;
+        default:
+          setLevel('Choice');
+          break;
+      }
+
+      if (ownRank > 0) {
+        setIsDisabled(true);
+      } else {
+        setIsDisabled(false);
+      }
+    }, [sportSelected]);
+
+    const handleChangeLevel = (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setLevel(event.target.value);
+    };
+
+    const handleSubmit = () => {
+      if (sportSelected === 'Choice' && level === 'Choice') {
+        console.log('Tu dois choisir un sport et un niveau');
+      } else if (sportSelected === 'Choice') {
+        console.log('Tu dois choisir un sport');
+      } else if (level === 'Choice') {
+        console.log('Tu dois choisir un niveau');
+      } else {
+        console.log('Tu est ' + level + ' en ' + sportSelected);
+        setIsDisabled(true);
+      }
+    };
+
+    return (
+      <form className="flex flex-col items-center form-control w-full px-6 gap-4">
+        <label className="label-text text-base self-start" htmlFor="sport">Now you can chose your level</label>
+        <select className="select select-bordered text-neutral-content w-full" value={level} onChange={handleChangeLevel} disabled={isDisabled} >
+          <option value='Choice' disabled className='font-bold italic'>Choose your level</option>
+          <option value='Beginner'>Beginner</option>
+          <option value='Intermediate'>Intermediate</option>
+          <option value='Confirmed'>Confirmed</option>
+        </select>
+        <button className="btn w-fit sm:mt-6" type="button" onClick={handleSubmit} disabled={isDisabled}>Save your level</button>
+      </form>
+    )
+}    
+
+export default SportRanking;
